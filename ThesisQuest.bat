7317ff6530585502eeb49f157a04e4e3be0cb3af
NanoYahya <48056838+YahyaShubbak@users.noreply.github.com>
2026-07-15 14:21:25 +0200

    Update ThesisQuest.bat

--- ThesisQuest.bat
+++ ThesisQuest.bat
@@ -1,8 +1,8 @@
 @echo off
 setlocal
-set "QUEST_ROOT=C:\Users\YaSh\Documents\GitHub\Thesis-Quest"
-set "THESIS_QUEST_PROJECT_ROOT=C:\Users\YaSh\Documents\GitHub\Thesis"
-set "THESIS_QUEST_STATE_DIR=C:\Users\YaSh\Documents\GitHub\Thesis-Quest\dashboard\projects\thesis-7682ac7f"
-set "THESIS_QUEST_OUTPUT_DIR=C:\Users\YaSh\Documents\GitHub\Thesis-Quest\dashboard\projects\thesis-7682ac7f\output"
-"C:\Users\YaSh\Documents\GitHub\Thesis-Quest\thesis-quest.bat" %*
+set "QUEST_ROOT=C:\Users\yshub\Documents\GitHub\Thesis-Quest"
+set "THESIS_QUEST_PROJECT_ROOT=C:\Users\yshub\Documents\GitHub\Thesis"
+set "THESIS_QUEST_STATE_DIR=C:\Users\yshub\Documents\GitHub\Thesis-Quest\dashboard\projects\thesis-1c8c1cfd"
+set "THESIS_QUEST_OUTPUT_DIR=C:\Users\yshub\Documents\GitHub\Thesis-Quest\dashboard\projects\thesis-1c8c1cfd\output"
+"C:\Users\yshub\Documents\GitHub\Thesis-Quest\thesis-quest.bat" %*
 endlocal
